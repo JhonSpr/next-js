3f7ca870dc08888446240e9e483af9c0fec5517e
Jhonspr <jhonreyes20hgmail.com>
2024-02-05 21:07:06 -0500

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 'use client'
 
-import Navigation from './components/Navigation'
+import Navigation from '@/components/Navigation'
 import '@/styles/globals.css'
 import '@/styles/navbar.css'
 import '@/styles/app.css'
