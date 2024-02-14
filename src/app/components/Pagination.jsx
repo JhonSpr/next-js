b0e4084307c75961f7ea985f1c3de75157a27316
Jhonspr <jhonreyes20hgmail.com>
2024-02-13 22:11:28 -0500

    ,.

--- src/app/components/Pagination.jsx
+++ src/app/components/Pagination.jsx
@@ -92,15 +92,15 @@ export default function Pagination({
       <nav aria-label='Page navigation example'>
         <ul className='inline-flex -space-x-px text-base h-10'>
           <li>
-            <button
-              href={`/directorio?${
+            <a
+              href={
                 current_page == 1 ? null : `${url + `page=${current_page - 1}`}`
-              }`}
+              }
               className={`flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                 current_page == 1 ? 'cursor-not-allowed' : ''
               } ${total_pages === 1 ? 'disable' : ''}`}>
               <FaArrowCircleLeft />
-            </button>
+            </a>
           </li>
           {pageNumber
             ?.map((i, index) => (
