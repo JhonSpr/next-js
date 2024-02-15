38b68b10142a12455c4020eeee394aecb0f995d4
Jhonspr <jhonreyes20hgmail.com>
2024-02-14 23:02:56 -0500

    .

--- src/app/directorio/animesFetch.jsx
+++ src/app/directorio/animesFetch.jsx
@@ -21,26 +21,42 @@ export default function AnimesFectching({ data }) {
           loading || theme == undefined ? 'skeleton-loader' : ''
         }`}
         key={index}>
-        <a href={`/${e.name.replace(/ /g, '-')}`}>
-          <div className='flex items-center rating__anime'>
-            <svg
-              className='w-3 h-3 text-yellow-300'
-              aria-hidden='true'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='currentColor'
-              viewBox='0 0 22 20'>
-              <path d='M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z' />
-            </svg>
-            <p className='ms-2 text-xlarge font-bold text-gray-900 dark:text-white'>
-              {e.rating || 5.2}
+        <div className='card__content'>
+          <a href={`/${e.name.replace(/ /g, '-')}`} className='column__image'>
+            <img src={e.image} alt='' className='image__anime' />
+            <strong className='name__anime'>{e.name}</strong>
+            <button
+              className='btn__anime'
+              onClick={() => {
+                location.assign(`/${e.name.replace(/ /, '-').toLowerCase()}`)
+              }}>
+              Ver Ahora
+            </button>
+          </a>
+          <div className='column__description'>
+            <span className='emitido__anime'>
+              {e?.emitido ?? 'desconocido'}
+            </span>
+            <span className='status__anime'>{e.estado}</span>
+            <span className='type__anime'>{e.tipo}</span>
+            <div className='flex items-center rating__anime'>
+              <svg
+                className='w-3 h-3 text-yellow-300'
+                aria-hidden='true'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='currentColor'
+                viewBox='0 0 22 20'>
+                <path d='M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z' />
+              </svg>
+              <p className='ms-2 text-xlarge font-bold text-gray-900 dark:text-white'>
+                {e.rating || 5.2}
+              </p>
+            </div>
+            <p className='description__anime'>
+              {e.descripcion ?? 'no hay descripcion disponible'}
             </p>
           </div>
-          <img src={e.image} alt='' className='image__anime' />
-          <span className='name__anime'>{e.name}</span>
-          <span className='year__anime'>{e.year}</span>
-          <span className='status__anime'>{e.estado}</span>
-          <span className='type__anime'>{e.tipo}</span>
-        </a>
+        </div>
       </article>
     )),
   ]
