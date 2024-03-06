5369cad8af38f4c5921dd726508e1d71808e4845
Jhonspr <jhonreyes20hgmail.com>
2024-03-05 21:30:13 -0500

    .

--- src/app/user/[idUser]/userPage.jsx
+++ src/app/user/[idUser]/userPage.jsx
@@ -133,10 +133,12 @@ const UserPage = () => {
         className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0'
         aria-label='Sidebar'>
         <div className='h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800'>
-          <Alert
-            isVisible={message}
-            message={'Avatar cambiado correctamente'}
-          />
+          {message && (
+            <Alert
+              isVisible={message}
+              message={'Avatar cambiado correctamente'}
+            />
+          )}
           <ul className='space-y-2 font-medium'>
             <li>
               <button
