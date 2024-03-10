1996cc883d4efef7fec266a42c478a647aaf3855
Jhonspr <jhonreyes20hgmail.com>
2024-03-10 16:50:19 -0500

    .

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -30,10 +30,10 @@ const Footer = () => {
           </div>
           <div className='grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3'>
             <div>
-              <h2 className='mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white'>
+              <h2 className='mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-white'>
                 Estados
               </h2>
-              <ul className='text-gray-500 dark:text-gray-400 font-medium'>
+              <ul className='text-white dark:text-gray-400 font-medium'>
                 <li className='mb-4'>
                   <a
                     href='/directorio?estados=en+emision'
