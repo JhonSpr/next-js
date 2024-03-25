65eb8882bd7a80cc5ad3d1c26b5da9ac1dbb02e6
Jhonspr <jhonreyes20hgmail.com>
2024-03-25 13:50:51 -0500

    .

--- src/app/components/popular__section.jsx
+++ src/app/components/popular__section.jsx
@@ -1,13 +1,25 @@
 'use client'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { FaStar } from 'react-icons/fa6'
 import { FcRating } from 'react-icons/fc'
 import { contextApp } from '../providers'
 
 const PopularSection = ({ MasVotados }) => {
   const { theme } = useContext(contextApp)
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 10)
+  }, [])
+
+  if (loading) {
+    return <div></div>
+  }
   return (
-    <section className={`section__popular ${theme == 'dark' ? 'dark' : ''}`}>
+    <section
+      className={`section__popular ${theme === 'dark' ? 'dark' : 'light'}`}>
       <div>
         <span className='title' style={{ color: '#fff' }}>
           <FcRating />
