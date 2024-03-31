cd056e61f2206e6173ac6b96252bfb54e7e0995a
“Jhonsr” <jhonreyes20h@gmail.com>
2024-03-31 00:38:30 -0500

    .

--- src/app/user/[idUser]/userPage.jsx
+++ src/app/user/[idUser]/userPage.jsx
@@ -344,13 +344,8 @@ const UserPage = () => {
 
                 <div className='last__child'>
                   <div className='file-input-container'>
-                    <label
-                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-                      htmlFor='file_input'>
-                      Cambiar avatar
-                    </label>
                     <input
-                      className='block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
+                      className='block w-full text-sm text-gray-900 border rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:placeholder-gray-400 input__file'
                       aria-describedby='file_input_help'
                       id='file_input'
                       type='file'
@@ -371,7 +366,7 @@ const UserPage = () => {
                   <button
                     onClick={handleUploadPhoto}
                     className='button__profile__page'>
-                    Guardar Cambios
+                    Cambiar Avatar
                   </button>
                 </div>
               </section>
