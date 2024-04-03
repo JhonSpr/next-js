9b3dc6fcafaded49ae63aee3d879d132bb399f34
“Jhonsr” <jhonreyes20h@gmail.com>
2024-04-02 22:51:44 -0500

    -....

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -433,7 +433,7 @@ function Navigation() {
                   href={
                     status === undefined || typeof location !== undefined
                       ? `${window.location.origin + route}`
-                      : null
+                      : `${window.location.origin + route}`
                   }
                   className={`block py-2 px-3 md:p-0 nav-link ${
                     status === 'coming soon' ? 'disabled' : ''
